5f3394cdf2f5e023b8766f68fc38b6586f4573a8
ABINAND SAIRAM M.B <abinandsairam@gmail.com>
2025-09-25 16:46:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1379113210-491f2",
+  "appId": "1:86198533753:web:2fb4f8527f66b23edddea8",
+  "apiKey": "AIzaSyD54NuFPaUWRH-cpWUuMvtNuwTWmocZA1I",
+  "authDomain": "studio-1379113210-491f2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "86198533753"
+};
