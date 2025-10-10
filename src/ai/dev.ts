e0349685695b8de3f2ad60db9ff3027a55f50e98
ABINAND SAIRAM M.B <abinandsairam@gmail.com>
2025-10-10 01:09:48 +0000

    import { config } from 'dotenv'; config();
    import '@/ai/flows/recommend

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -2,7 +2,5 @@
 import { config } from 'dotenv';
 config();
 
-import '@/ai/flows/translate-prediction-results.ts';
-import '@/ai/flows/recommend-crop-flow.ts';
 import '@/ai/flows/recommend-treatment-flow.ts';
 import '@/ai/flows/identify-pest-disease-flow.ts';
